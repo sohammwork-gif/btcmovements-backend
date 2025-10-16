a85abfdab7ec81d681687e216a42c5b3a6d8cb74
sohammwork-gif <sohamm.work@gmail.com>
2025-10-16 13:59:50 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
+import express from 'express';
+import axios from 'axios';
+import cors from 'cors';
 
 const app = express();
 app.use(cors());
